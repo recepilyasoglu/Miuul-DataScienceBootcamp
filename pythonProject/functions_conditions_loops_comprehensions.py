d08a5688096a80e24ebf00154560dc59f507a5a4
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-01-02 19:01:21 +0300

    Pyhton Programming for Data Science

--- pythonProject/functions_conditions_loops_comprehensions.py
+++ pythonProject/functions_conditions_loops_comprehensions.py
@@ -230,7 +230,69 @@ for salary in salaries:
 [new_salary(salary * 2) if salary < 3000 else new_salary(salary * 0.2) for salary in salaries]
 
 
-students = ["Johns", "Mark", "Vanessa", "Mariam"]
+students = ["John", "Mark", "Vanessa", "Mariam"]
 
 students_no = ["John", "Vanessa"]
+
+[student.lower() if student in students_no else student.upper() for student in students]
+
+[student.upper() if student not in students_no else student.lower() for student in students]
+
+
+# Dict Comprehensions
+dictionary = {"a" : 1,
+              "b" : 2,
+              "c" : 3,
+              "d" : 4
+              }
+
+dictionary.keys()
+dictionary.values()
+dictionary.items()
+#square of values
+{k: v ** 2 for (k, v) in dictionary.items()}
+
+{k.upper(): v for (k, v) in dictionary.items()}
+
+{k.upper(): v*2 for (k, v) in dictionary.items()}
+
+
+# Uygulama: Çift sayıalrın karesi alınarak bir sözlüğe eklenmek istenmektedir
+# Key'ler orjinal değerler value'lar ise değiştirilmiş değerler olacak
+
+numbers = range(10)
+new_dict = {}
+
+for i in numbers:
+    if i % 2 == 0:
+        new_dict[i] = i ** 2
+
+{i: i ** 1 for i in numbers if i % 2 == 0}
+
+
+# Applications of List & Dict Comprehensions
+## Uygulama: Bir veri setindeki değişken isimlerini değiştirmek
+
+#before : ["total","speeding","alcohol","not_distracted","no_previous","ins_premium","in_losses","abbrev"]
+#after : ["TOTAL","SPEEDİNG","ALCOHOL","NOT_DİSTRACTED","NO_PREVİOUS","İNS_PREMİUM","İNS_LOSSES","ABBREV"]
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+for col in df.columns:
+    print(col.upper())
+
+A = []
+
+for col in df.columns:
+    A.append(col.upper())
+
+df.columns = A
+
+# with List Comprehensions
+df = sns.load_dataset("car_crashes")
+df.columns
+A = []
+df.columns = [col.upper() for col in df.columns]
 
