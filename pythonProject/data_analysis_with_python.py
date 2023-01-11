94f603fca9bf4f9ee9a16c764e44ec71faa7711d
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-01-11 11:39:26 +0300

    Data Analysis with Pyhton

--- pythonProject/data_analysis_with_python.py
+++ pythonProject/data_analysis_with_python.py
@@ -1,17 +1,18 @@
 # NumPy
 
 import numpy as np
-a = [1,2,3,4]
-b = [2,3,4,5]
+
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
 
 ab = []
 
 for i in range(0, len(a)):
     ab.append(a[i] * b[i])
 
-#with numpy
-a = np.array([1,2,3,4])
-b = np.array([2,3,4,5])
+# with numpy
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
 a * b
 
 import numpy as np
@@ -19,8 +20,8 @@ import numpy as np
 np.array([1, 2, 3, 4, 5])
 type(np.array([1, 2, 3, 4, 5]))
 np.zeros(10, dtype=int)
-np.random.randint(0, 10, size=10) #0 ile 10 arasında rastgele 10 integer
-np.random.normal(10, 4, (3,4)) #ortalaması 10, standart sapması 4 olan, 3'e 4 array oluşturma
+np.random.randint(0, 10, size=10)  # 0 ile 10 arasında rastgele 10 integer
+np.random.normal(10, 4, (3, 4))  # ortalaması 10, standart sapması 4 olan, 3'e 4 array oluşturma
 
 # ndim: boyut sayısı
 # shape: boyut bilgisi
@@ -35,8 +36,86 @@ a.dtype
 
 # reshaping
 import numpy as np
+
 np.random.randint(1, 10, size=9)
 np.random.randint(1, 10, size=9).reshape(3, 3)
 
 ar = np.random.randint(1, 10, size=9)
 ar.reshape(3, 3)
+
+# Index Selection
+import numpy as np
+
+a = np.random.randint(10, size=10)
+a[0]
+a[0:5]
+a[0] = 999
+
+m = np.random.randint(10, size=(3, 5))
+m[0, 0]
+m[1, 1]
+m[2, 3]
+
+m[2, 3] = 999
+
+m[2, 3] = 2.9
+
+m[:, 0]
+m[1, :]
+m[0:2, 0:3]
+
+# Fancy Index
+import numpy as np
+
+v = np.arange(0, 30, 3)
+v[1]
+v[4]
+
+catch = [1, 2, 3]
+
+v[catch]
+
+# Conditions on Numpy
+import numpy as np
+v = np.array([1, 2, 3, 4, 5])
+
+# with classic loop
+ab = []
+for i in v:
+    if i < 3:
+        ab.append(i)
+
+#with Numpy
+v < 3
+
+v[v < 3]
+v[v > 3]
+v[v != 3]
+v[v == 3]
+v[v >= 3]
+
+# Mathematical Operations
+import numpy as np
+v = np.array([1, 2, 3, 4, 5])
+
+v / 5
+v * 5 / 10
+v ** 2
+v - 1
+
+np.subtract(v, 1)
+np.add(v, 1)
+np.mean(v)
+np.max(v)
+np.min(v)
+np.sum(v)
+np.var(v)
+
+#İki bilinmeyenli denklem çözümü
+# 5+x0 + x1 = 12
+# x0 + 3*x1 = 10
+
+a = np.array([[5, 1], [1, 3]])
+b = np.array([12, 10])
+
+np.linalg.solve(a, b)
