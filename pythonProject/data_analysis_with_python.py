7b556d48e8affabafc308a2ee49dea3f16142d1e
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-01-17 19:41:54 +0300

    Condition Selection

--- pythonProject/data_analysis_with_python.py
+++ pythonProject/data_analysis_with_python.py
@@ -238,3 +238,27 @@ df.loc[0:3, "age"]
 
 col_names = ["age", "embarked", "alive"]
 df.loc[0:3, col_names]
+
+#Condition Selection
+import pandas as pd
+import seaborn as sns
+pd.set_option("display.max.columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df[df["age"] > 50].head()
+df[df["age"] > 50]["age"].count()
+
+df.loc[df["age"] > 50, ["age", "class"]].head()
+
+#parentheses are used for more than one condition
+df.loc[(df["age"] > 50) & (df["sex"] == "male"), ["age", "class"]].head()
+
+df["embark_town"].value_counts()
+
+df_new  = df.loc[(df["age"] > 50) & (df["sex"] == "male")
+       & ((df["embark_town"] == "Cherbourg") | (df["embark_town"] == "Southampton")),
+       ["age", "class", "embark_town"]]
+
+df_new["embark_town"].value_counts()
+
