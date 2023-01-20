df442225ae60f7dff4b39d8b0b8f47eae27dbe08
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-01-20 06:17:49 +0300

    Analysis of Numerical Variables

--- pythonProject/data_analysis_with_python-eda.py
+++ pythonProject/data_analysis_with_python-eda.py
@@ -132,3 +132,45 @@ def cat_summary(dataframe, col_name, plot=False):
             plt.show(block=True)
 
 cat_summary(df, "adult_male", plot=True)
+
+
+
+# Analysis of Numerical Variables
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option("display.max_columns", None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df[["age", "fare"]].describe().T
+
+# how do i select numeric values from dataset ?
+num_cols = [col for col in df.columns if df[col].dtypes in ["int64", "float64"]]
+# if not in cat_cols, select them
+num_cols = [col for col in num_cols if col not in cat_cols]
+
+def num_summary(dataframe, numerical_col):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+num_summary(df, "age")
+
+for col in num_cols:
+    num_summary(df, col)
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+num_summary(df, "age", plot=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
