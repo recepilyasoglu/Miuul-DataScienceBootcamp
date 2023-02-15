61fa5800e68c3d41f543354c69bc4698a14d6c61
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-02-15 04:19:35 +0300

    Pandas Exercises

--- pythonProject/pandas_exercises.py
+++ pythonProject/pandas_exercises.py
@@ -34,3 +34,25 @@ df[(df["age"] < 30) & (df['sex'] == "female")]
 
 # Task 10: Show the information of passengers whose Fare is over 500 or 70 years old
 df[(df["fare"] > 500) | (df["age"] > 70)]
+
+# Task 11: Find the sum of the null values in each variable.
+df.isnull().sum()
+
+# Task 12: Extract the variable who from the dataframe.
+df.drop(columns="who", inplace=True)
+df.columns
+
+# Task 13: Fill the empty values in the deck variable with the most repeated value (mode) of the deck variable.
+
+# Task 14: Fill in the blank values in the age variable with the median of the age variable.
+
+# Task 15: Find the sum, count, mean values of the pclass and gender variables of the survived variable.
+
+# Task 16: Write a function that returns 1 for those under 30 and 0 for those above or equal to 30. titanic data using the function you wrote
+#create a variable named age_flag in the set. (use apply and lambda constructs)
+
+# Task 17: Define the Tips dataset from the Seaborn library.
+
+# Task 18: Find the sum, min, max and average of the total_bill values according to the categories (Dinner, Lunch) of the Time variable.
+
+# Task 19: Find the sum, min, max and average of total_bill values by days and time.
