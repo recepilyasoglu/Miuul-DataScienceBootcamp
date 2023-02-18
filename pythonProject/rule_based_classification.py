c87377af7e7f2ded39d02a6f448157d5eac41750
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-02-18 03:54:48 +0300

    Rule Based Classification

--- pythonProject/rule_based_classification.py
+++ pythonProject/rule_based_classification.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 ### Calculating Lead-Based Returns with Rule-Based Classification
 
+## Task 1: Answer the Following Questions
 # Question 1: Read the persona.csv file and show the general information about the dataset.
 data = pd.read_csv("persona.csv")
 df = data.copy()
@@ -27,12 +28,29 @@ df["PRICE"].value_counts()
 df["COUNTRY"].value_counts()
 
 # Question 6: How much was earned in total from sales by country?
-
+df.groupby("COUNTRY").agg({"PRICE": "sum"}).sort_values("PRICE", ascending=False)
 
 # Question 7: What are the sales numbers by SOURCE types?
+df.groupby("SOURCE").agg({"PRICE": "sum"})
 
 # Question 8: What are the PRICE averages by country?
+df.groupby("COUNTRY").agg({"PRICE": "mean"}).sort_values("PRICE", ascending=False)
 
-# Question 9: What are the PRICE averages according to SOURCEs?
+# Question 9: What are the PRICE averages according to SOURCE's?
+df.groupby("SOURCE").agg({"PRICE": "mean"})
 
 # Question 10: What are the PRICE averages in the COUNTRY-SOURCE breakdown?
+df.groupby(["COUNTRY", "SOURCE"]).agg({"PRICE": "mean"})
+
+
+## Task 2: What are the average earnings in breakdown of COUNTRY, SOURCE, SEX, AGE?
+agg_df = df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"]).agg({"PRICE": "mean"})
+
+## Task 3: Sort the output according to PRICE.
+
+# To better see the output in the previous question, apply the sort_values method in descending order of PRICE.
+# Save the output as agg_df.
+agg_df = agg_df.sort_values("PRICE", ascending=False)
+
+## Task 4: Convert the names in the index to variable names
+agg_df.reset_index(inplace=True)
