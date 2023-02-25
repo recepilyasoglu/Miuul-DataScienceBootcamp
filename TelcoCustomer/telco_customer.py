12787abb9ff877e02e8f3808baa486944766da0f
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-02-25 03:01:31 +0300

    Analysis of Telco Cuatomer Dataset

--- TelcoCustomer/telco_customer.py
+++ TelcoCustomer/telco_customer.py
@@ -8,13 +8,14 @@ data = pd.read_csv("Telco-Customer-Churn.csv")
 df = data.copy()
 df.head()
 
+
 # 2- Telco Customer Churn data set;
-#• Shape
-#• Dtypes
-#• Head, Tail
-#• Missing Value
-#• Describe
-#obtain information.
+# • Shape
+# • Dtypes
+# • Head, Tail
+# • Missing Value
+# • Describe
+# obtain information.
 
 df.shape
 df.dtypes
@@ -25,8 +26,8 @@ df.describe().T
 
 
 # 3-Navigating in the Gender column of the dataset and "Male" of the gender column
-#It prints 0 when it encounters the class, 1 when it encounters the opposite situation.
-#Type comphrension and create a new file named "num_gender". assign to variable
+# It prints 0 when it encounters the class, 1 when it encounters the opposite situation.
+# Type comphrension and create a new file named "num_gender". assign to variable
 
 #def check_gender(gender):
 #        if gender == "Male":
@@ -38,3 +39,29 @@ df.describe().T
 
 df["num_gender"] = [0 if col == "Male" else 1 for col in df.gender]
 
+
+# 4- For the "Yes" class within the classes of the "PaperlessBilling" column
+# Write a lambda function that prints "Yes" , "No" otherwise. And
+# put the result in your newly created column named "NEW_PaperlessBilling"
+# print it. (you can use lambda function with apply)
+
+df["NEW_PaperlessBilling"] = df["PaperlessBilling"].apply(lambda x: "Evt" if x == "Yes" else "Hyr")
+
+
+# 5- Those whose class is "Yes" within the scope of columns containing "Online" in the data set, "Yes", "No"
+# Write code that will reformat the classes as "No", otherwise "No_Internet".
+# Note: In order not to encounter if elif else syntax error in lambda, use another def function.
+# Repeat classes as "Yes" for "yes", "No" for "No", otherwise "No_Internet".
+# You can create the function to format it outside and apply this function inside the lambda.
+
+
+def check_online(online):
+    for i in online:
+        if online[i] == "Yes":
+            return "Evet"
+        elif online[i] == "No":
+            return "Hayır"
+        else:
+            return "Interneti_yok"
+
+df.filter(like="Online").apply(lambda x: check_online(x), axis=1)
