5c572ccaa8a6228e263b35e947d90ed52cd3f787
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-01 19:17:45 +0300

    Rule Based Classification

--- Miuul-DataScienceBootcamp/Tasks/2.hafta/rule_based_classification.py
+++ Miuul-DataScienceBootcamp/Tasks/2.hafta/rule_based_classification.py
@@ -11,7 +11,7 @@ pd.set_option("display.max_columns", None)
 
 ## Task 1: Answer the Following Questions
 # Question 1: Read the persona.csv file and show the general information about the dataset.
-data = pd.read_csv(r"C:\Users\reco1\PycharmProjects\Miuul-DataScienceBootcamp\Tasks\2.hafta\persona.csv")
+data = pd.read_csv("persona.csv")
 df = data.copy()
 df.head()
 df.info()
