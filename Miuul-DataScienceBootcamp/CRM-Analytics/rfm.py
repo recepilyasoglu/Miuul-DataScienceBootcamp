7487d4d7ee37a393611b4e6baeeaa765bbcab2f9
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-04 01:46:23 +0300

    Customer Segmentation with RFM

--- Miuul-DataScienceBootcamp/CRM-Analytics/rfm.py
+++ Miuul-DataScienceBootcamp/CRM-Analytics/rfm.py
@@ -25,10 +25,42 @@
 # CustomerID: Eşsiz Müşteri numarası
 # Country: Ülke ismi. Müşterinin yaşadığı ülke
 
+
+## 2. Data Understanding
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+data = pd.read_excel(r"C:\Users\reco1\PycharmProjects\Miuul-DataScienceBootcamp\CRM-Analytics\online_retail_II.xlsx", sheet_name="Year 2009-2010")
+df = data.copy()
+df.head()
+df.shape
+df.isnull().sum()
+
+# eşsiz ürün  sayısı nedir?
+df["Description"].nunique()
+
+df["Description"].value_counts().head()
+
+df.groupby("Description").agg({"Quantity": "sum"}).head()
+
+# her bir üründen toplam ne kadar sipariş verildiği
+df.groupby("Description").agg({"Quantity": "sum"}).sort_values("Quantity", ascending=False).head()
+
+# eşsiz fatura sayısı
+df["Invoice"].nunique()
+
+# her bir ürünün tam fiyatı
+# kaç tane satıldığı * fiyatı
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+# Invoice başına toplan ne kadar ödendiği
+df.groupby("Invoice").agg({"TotalPrice": "sum"}).head()
+
+
 
 
 
