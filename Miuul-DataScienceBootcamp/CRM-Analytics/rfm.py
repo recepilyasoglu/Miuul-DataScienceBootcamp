ebb1f72eeac7d2f0bdb450ffd772125ae5bdb1e0
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-03 18:43:24 +0300

    CRM Analytics

---
+++ Miuul-DataScienceBootcamp/CRM-Analytics/rfm.py
@@ -0,0 +1,19 @@
+##################################
+# Customer Segmentation with RFM #
+##################################
+
+# 1. Business Problem
+# 2. Data Understanding
+# 3. Data Preparation
+# 4. Calculating RFM Metrics
+# 5. Calculating RFM Scores
+# 6. Creating & Analysing RFM Segments
+# 7. Functionalization of the whole process
+
+
+## 1. Business Problem
+# Bir e-ticaret şirketi müşterilerini segmentlere ayırıp bu segmentlere göre
+# pazarlama stratejileri belirlemek istiyor.
+
+
+
