929651dc9f13f8cda3798d69b6030db08a78fdc6
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-05 11:18:48 +0300

    Functionalization of the Whole Process

--- Miuul-DataScienceBootcamp/CRM-Analytics/cltv.py
+++ Miuul-DataScienceBootcamp/CRM-Analytics/cltv.py
@@ -102,3 +102,40 @@ cltv_c.sort_values(by="cltv", ascending=False).head()
 cltv_c.groupby("segment").agg({"count", "mean", "sum"})
 
 cltv_c.to_csv("cltv_c.csv")
+
+
+# 9. BONUS: Functionalization of the whole process (Tüm İşlemlerin Fonksiyonlaştırılması)
+
+def create_cltv_c(dataframe, profit=0.10):
+
+    # Veriyi Hazırlama
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe.dropna(inplace=True)
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    cltv_c = dataframe.groupby("Customer ID").agg({"Invoice": lambda x: x.nunique(),
+                                                   "Quantity": lambda x: x.sum(),
+                                                   "TotalPrice": lambda x: x.sum()})
+    cltv_c.columns = ["total_transaction", "total_unit", "total_price"]
+    # avg_order_value
+    cltv_c["avg_order_value"] = cltv_c["total_price"] / cltv_c["total_transaction"]
+    #purchase_frequency
+    cltv_c["purchase_frequency"] = cltv_c["total_transaction"] / cltv_c.shape[0]
+    # repeat rate & churn rate
+    repeat_rate = cltv_c[cltv_c["total_transaction"] > 1].shape[0] / cltv_c.shape[0]
+    churn_rate = 1 - repeat_rate
+    # profit_margin
+    cltv_c["profit_margin"] = cltv_c["total_price"] * 0.10
+    # Customer Value
+    cltv_c["customer_value"] = cltv_c["avg_order_value"] * cltv_c["purchase_frequency"]
+    # Customer Lifetime Value
+    cltv_c["cltv"] = (cltv_c["customer_value"] / churn_rate) * cltv_c["profit_margin"]
+    cltv_c.sort_values(by="cltv", ascending=False).head()
+    # Segment
+    cltv_c["segment"] = pd.qcut(cltv_c["cltv"], 4, labels=["D", "C", "B", "A"])
+
+    return cltv_c
+
+df = df_.copy()
+
+clv = create_cltv_c(df)
