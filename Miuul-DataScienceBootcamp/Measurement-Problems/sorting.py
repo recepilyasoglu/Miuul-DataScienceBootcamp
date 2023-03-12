eb0ad1dcaf5e2e3ce236eed9bd036fee6213e653
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-12 13:28:18 +0300

    Sorting by Rating, Comment and Purchase

--- Miuul-DataScienceBootcamp/Measurement-Problems/sorting.py
+++ Miuul-DataScienceBootcamp/Measurement-Problems/sorting.py
@@ -3,6 +3,8 @@
 # Uygulama: Kurs Sıralama
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+import math
+import scipy.stats as st
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.expand_frame_repr', False)
@@ -57,3 +59,37 @@ df.sort_values('weighted_sorting_score', ascending=False).head(20)
 
 df[df["course_name"].str.contains("Veri Bilimi")].sort_values("weighted_sorting_score", ascending=False).head(20)
 
+
+# Bayesian Average Rating Score
+
+## Sorting Products with 5 Star Rated
+## Sorting Products According to Distribution of 5 Star Rating
+
+# puan dağılımlarının üzerinden ağırlıklı bir şekilde olasılıksal ortalama hesaplar
+def bayesian_average_rating(n, confidence=0.95):  # n = girilecek olan yıldızların ve bu yıldızlara ait gözlenme frakanslarını ifade etmektedir
+    if sum(n) == 0:
+        return 0
+    K = len(n)
+    z = st.norm.ppf(1 -(1 - confidence) / 2)
+    N = sum(n)
+    first_part = 0.0
+    second_part = 0.0
+    for k, n_k in enumerate(n):
+        first_part += (k + 1) * (n[k] + 1) / (N + K)
+        second_part += (k + 1) * (k + 1) * (n[k] + 1) / (N + K)
+    score = first_part - z * math.sqrt((second_part - first_part * first_part) / (N + K + 1))
+    return score
+
+df.head()
+
+df["bar_score"] = df.apply(lambda x: bayesian_average_rating(x[["1_point",
+                                                                "2_point",
+                                                                "3_point",
+                                                                "4_point",
+                                                                "5_point"]]), axis=1)
+
+df.sort_values("weighted_sorting_score", ascending=False).head(20)
+df.sort_values("bar_score", ascending=False).head(20)
+
+# aralarındaki farkın sebebi düşük puan miktarının diğer kursa göre az olması veya olmaması
+df[df["course_name"].index.isin([5, 1])].sort_values("bar_score", ascending=False)
