67d4c44ab46ec21e596abc80b2096cca7c8333be
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-13 17:30:53 +0300

    Rating Product & Sorting Reviews in Amazon

--- Miuul-DataScienceBootcamp/Tasks/4.hafta/RatingProduct-SortingReviews-Amazon.py
+++ Miuul-DataScienceBootcamp/Tasks/4.hafta/RatingProduct-SortingReviews-Amazon.py
@@ -49,15 +49,39 @@
 ###################################################
 # Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
 ###################################################
+import pandas as pd
+import math
+import scipy.stats as st
+from sklearn.preprocessing import MinMaxScaler
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option("display.width", 500)
+
+df = pd.read_csv("Tasks/4.hafta/amazon_review.csv")
+df.head()
+df.shape
+df.describe().T
+df.dtypes
+
+df["overall"].mean()
 
 ###################################################
 # Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
 ###################################################
 
+df["reviewTime"].max()
+df["day_diff"].head()
 
+def time_based_weighted_average(df, w1=28, w2=26, w3=24, w4=22):
+    return df.loc[df["day_diff"] <= 30, "overall"].mean() * w1 / 100 + \
+           df.loc[(df["day_diff"] > 30) & (df["day_diff"] <= 90), "overall"].mean() * w2 / 100 + \
+           df.loc[(df["day_diff"] > 90) & (df["day_diff"] <= 180), "overall"].mean() * w3 / 100 + \
+           df.loc[df["day_diff"] > 180, "overall"].mean() * w4 / 100
 
-
+time_based_weighted_average(df)
 ###################################################
 # Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
 ###################################################
@@ -72,14 +96,69 @@
 # up, helpful demektir.
 # veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
 
+df.columns
+df["helpful_no"] = df["total_vote"] - df["helpful_yes"]
 
 ###################################################
 # Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
 ###################################################
+def score_pos_neg_diff(up, down):
+    return up - down
+
+def score_average_rating(up, down):
+    if up + down == 0:
+        return 0
+    return up / (up + down)
+
+def wilson_lower_bound(up, down, confidence=0.95):
+    """
+    Wilson Lower Bound Score hesapla
+
+    - Bernoulli parametresi p için hesaplanacak güven aralığının alt sınırı WLB skoru olarak kabul edilir.
+    - Hesaplanacak skor ürün sıralaması için kullanılır.
+    - Not:
+    Eğer skorlar 1-5 arasıdaysa 1-3 negatif, 4-5 pozitif olarak işaretlenir ve bernoulli'ye uygun hale getirilebilir.
+    Bu beraberinde bazı problemleri de getirir. Bu sebeple bayesian average rating yapmak gerekir.
+
+    Parameters
+    ----------
+    up: int
+        up count
+    down: int
+        down count
+    confidence: float
+        confidence
+
+    Returns
+    -------
+    wilson score: float
+
+    """
+    n = up + down
+    if n == 0:
+        return 0
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    phat = 1.0 * up / n
+    return (phat + z * z / (2 * n) - z * math.sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
 
 
+df["score_pos_neg_diff"] = df.apply(lambda x: score_pos_neg_diff(x["helpful_yes"],
+                                                                 x["helpful_no"]), axis=1)
+
+df["score_average_rating"] = df.apply(lambda x: score_average_rating(x["helpful_yes"],
+                                                                     x["helpful_no"]), axis=1)
+
+df["wilson_lower_bound"] = df.apply(lambda x: wilson_lower_bound(x["helpful_yes"],
+                                                                 x["helpful_no"]), axis=1)
+
+df.head(20)
 ##################################################
 # Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
 ###################################################
 
+df.sort_values("wilson_lower_bound", ascending=False).head(20)
+
+df.sort_values("score_average_rating", ascending=False).head(20)
+
+df.sort_values("score_average_rating", ascending=False).head(20)
 
