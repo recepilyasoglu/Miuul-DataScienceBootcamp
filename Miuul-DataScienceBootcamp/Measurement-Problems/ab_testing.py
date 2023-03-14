f547cb8aeddff8da2b6174b8f25c6bd86b9480c3
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-14 17:14:19 +0300

    Correlation

--- Miuul-DataScienceBootcamp/Measurement-Problems/ab_testing.py
+++ Miuul-DataScienceBootcamp/Measurement-Problems/ab_testing.py
@@ -3,9 +3,13 @@
 ###############################
 
 import itertools
+
+import matplotlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Qt5Agg")
 import seaborn as sns
 import statsmodels.stats.api as sms
 from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, \
@@ -15,7 +19,7 @@ from statsmodels.stats.proportion import proportions_ztest
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
-
+pd.set_option("display.width", 500)
 
 # Sampling (Örnekleme)
 populasyon = np.random.randint(0, 80, 10000)
@@ -42,9 +46,51 @@ orneklem10 = np.random.choice(a=populasyon, size=100)
  orneklem6.mean() + orneklem7.mean() + orneklem8.mean() + orneklem9.mean() + orneklem10.mean()) / 10
 
 
+# Descriptive Statistics (Betimsel İstatistikler)
+df = sns.load_dataset("tips")
+df.describe().T
 
 
+# Confidence Intervals (Güven Aralıkları)
+df = sns.load_dataset("tips")
+df.describe().T
 
+df.head()
+
+# TotalBill değişkeninin güven aralığı nedir?
+sms.DescrStatsW(df["total_bill"]).tconfint_mean()
+# tip değişkeninin güven aralığı nedir?
+sms.DescrStatsW(df["tip"]).tconfint_mean()
+
+# Titanic Veri Setindeki Sayısal Değişkenler için Güven Aralığı
+df = sns.load_dataset("titanic")
+df.describe().T
+sms.DescrStatsW(df["age"].dropna()).tconfint_mean()
+
+sms.DescrStatsW(df["fare"].dropna()).tconfint_mean()
+
+
+# Correlation (Korelasyon)
+
+# Bahşiş veri seti:
+# total_bill: yemeğin toplam fiyatı (bahşiş ve vergi dahil)
+# tip: bahşiş
+# sex: ücreti ödeyen kişinin cinsiyeti (0=male, 1=female)
+# smoker: grupta sigara içen var mı? (0=No, 1=Yes)
+# day: gün (3=Thur, 4=Fri, 5=Sat, 6=Sun)
+# time: ne zaman? (0=Day, 1=Night)
+# size: grupta kaç kişi var?
+
+df = sns.load_dataset("tips")
+df.head()
+
+# Verilen Bahşişler ile Ödenen Hesap arasındaki korelasyon var mı ?
+df["total_bill"] = df["total_bill"] - df["tip"]
+
+df.plot.scatter("tip", 'total_bill')
+plt.show()
+
+df["tip"].corr(df["total_bill"])  # Yorum: Pozitif yönlü orta şiddetli bir ilişki vardır.
 
 
 
