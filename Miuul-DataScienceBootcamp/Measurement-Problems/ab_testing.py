a3d5f43fbaf344a5e8232b3c4aa9091e6e820c25
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-15 15:17:14 +0300

    ANOVA - Analysis of Variance

--- Miuul-DataScienceBootcamp/Measurement-Problems/ab_testing.py
+++ Miuul-DataScienceBootcamp/Measurement-Problems/ab_testing.py
@@ -345,7 +345,76 @@ test_stat, pvalue = proportions_ztest(count=[female_succ_count, male_succ_count]
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 
 
+######################################################
+# ANOVA (Analysis of Variance)
+######################################################
 
+# İkiden fazla grup ortalamasını karşılaştırmak için kullanılır.
+
+df = sns.load_dataset("tips")
+df.head()
+
+df.groupby("day")["total_bill"].mean()
+
+# 1. Hipotezleri kur
+
+# HO: m1 = m2 = m3 = m4
+# Grup ortalamaları arasında fark yoktur.
+
+# H1: .. fark vardır
+
+# 2. Varsayım kontrolü
+
+# Normallik varsayımı
+# Varyans homojenliği varsayımı
+
+# Varsayım sağlanıyorsa one way anova
+# Varsayım sağlanmıyorsa kruskal
+
+
+# H0: Normal dağılım varsayımı sağlanmaktadır.
+
+for group in list(df["day"].unique()):
+    pvalue = shapiro(df.loc[df["day"] == group, 'total_bill'])[1]
+    print(group, "p-value: %.4f" % pvalue)
+
+
+# H0: Varyans homojenliği varsayımı sağlanmaktadır.
+
+test_stat, pvalue = levene(df.loc[df["day"] == "Sun", "total_bill"],
+                           df.loc[df["day"] == "Sat", "total_bill"],
+                           df.loc[df["day"] == "Thur", "total_bill"],
+                           df.loc[df["day"] == "Fri", "total_bill"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+
+# 3. Hipotez testi ve p-value yorumu
+
+# Hiç biri sağlamıyor.
+
+df.groupby("day").agg({"total_bill": ["mean", "median"]})
+
+
+# HO: Grup ortalamaları arasında ist ol anl fark yoktur
+
+# parametrik anova testi:
+f_oneway(df.loc[df["day"] == "Thur", "total_bill"],
+         df.loc[df["day"] == "Fri", "total_bill"],
+         df.loc[df["day"] == "Sat", "total_bill"],
+         df.loc[df["day"] == "Sun", "total_bill"])
+
+# Nonparametrik anova testi:
+kruskal(df.loc[df["day"] == "Thur", "total_bill"],
+        df.loc[df["day"] == "Fri", "total_bill"],
+        df.loc[df["day"] == "Sat", "total_bill"],
+        df.loc[df["day"] == "Sun", "total_bill"])
+
+
+# İki grup arasında farklılık bulduk ama bu farklılık hangi gruptan kaynaklanıyor?
+from statsmodels.stats.multicomp import MultiComparison
+comparison = MultiComparison(df['total_bill'], df['day'])
+tukey = comparison.tukeyhsd(0.05)
+print(tukey.summary())
 
 
 
