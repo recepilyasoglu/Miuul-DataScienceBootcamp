0c65af8a9133f92fde1ed731ee9bd2ebfa48f388
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-17 17:26:52 +0300

    Data Preprocessing

--- Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
+++ Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
@@ -38,6 +38,39 @@ def retail_data_prep(dataframe):
     return dataframe
 
 df = retail_data_prep(df)
+
+# ilk olarak, girilen değişkenin %1 çeyrek değerini ve %99 çeyrek değerini hesaplayıp değişkenler de bunu bir tut,
+# sonra diyoruz ki %99 luk çeyrek  değerden 1,5 IQR uzaklıktaki değer benim üst limitimdir,
+# %1 lik çeyrek  değerden 1,5 IQR yakınındaki değer benim alt limitimdir.
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+# yukarıda hesaplanan değere göre baskılama yapmak
+# ilgili değişkenden küçük olanlar var mı bak, yukarıda belirlenen low limitten küçük ve ilgili değişkeni, low limite tekrar ata yani baskıla
+# aynı şeyi up limit için de yap
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+# yeniden çağırıp yukarıdaki fonksiyonları da ekliyoruz bu sefer
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+df = retail_data_prep(df)
+df.isnull().sum()
 df.describe().T
 
 
