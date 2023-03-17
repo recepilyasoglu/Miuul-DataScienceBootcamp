c39adf29d2bbd3ad5607f29f2ac266a0a3d3d644
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-17 16:53:14 +0300

    Data Preprocessing

---
+++ Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
@@ -0,0 +1,44 @@
+############################################
+# ASSOCIATION RULE LEARNING (BİRLİKTELİK KURALI ÖĞRENİMİ)
+############################################
+
+# 1. Veri Ön İşleme
+# 2. ARL Veri Yapısını Hazırlama (Invoice-Product Matrix)
+# 3. Birliktelik Kurallarının Çıkarılması
+# 4. Çalışmanın Scriptini Hazırlama
+# 5. Sepet Aşamasındaki Kullanıcılara Ürün Önerisinde Bulunmak
+
+############################################
+# 1. Veri Ön İşleme
+############################################
+
+# !pip install mlxtend
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+# çıktının tek bir satırda olmasını sağlar.
+pd.set_option('display.expand_frame_repr', False)
+from mlxtend.frequent_patterns import apriori, association_rules
+
+df_ = pd.read_excel("Recommender-Systems/online_retail_II.xlsx", sheet_name="Year 2010-2011")
+df = df_.copy()
+df.head()
+
+df.describe().T  # eksi değerler var olmaması lazım, ve aykırı değerler var çeyrekliklere bakılınca anlaşılıyor
+df.isnull().sum()
+df.shape
+
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    return dataframe
+
+df = retail_data_prep(df)
+df.describe().T
+
+
+
