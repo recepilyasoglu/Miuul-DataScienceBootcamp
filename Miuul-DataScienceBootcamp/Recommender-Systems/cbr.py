7be69a264e9b79e8652a012285a72a15689525d3
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-20 10:55:01 +0300

    Content Based Recommendation

---
+++ Miuul-DataScienceBootcamp/Recommender-Systems/cbr.py
@@ -0,0 +1,34 @@
+#############################
+# Content Based Recommendation (İçerik Temelli Tavsiye)
+#############################
+
+#############################
+# Film Overview'larına Göre Tavsiye Geliştirme
+#############################
+
+# 1. TF-IDF Matrisinin Oluşturulması
+# 2. Cosine Similarity Matrisinin Oluşturulması
+# 3. Benzerliklere Göre Önerilerin Yapılması
+# 4. Çalışma Scriptinin Hazırlanması
+
+
+#####################################
+# 1. TF-IDF Matrisinin Oluşturulması
+####################################
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.expand_frame_repr', False)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+df_ = pd.read_csv("Recommender-Systems/movies_metadata.csv", low_memory=False)
+df = df_.copy()
+df.head()
+df.shape
+
+
+
+
+
