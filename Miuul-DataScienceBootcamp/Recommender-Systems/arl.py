0dbc7f3d6599ae4cf0e6e5febcf7082efc2c0337
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-20 07:57:37 +0300

    Preparing ARL Data Structures

--- Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
+++ Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
@@ -14,6 +14,7 @@
 
 # !pip install mlxtend
 import pandas as pd
+
 pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 500)
@@ -37,7 +38,9 @@ def retail_data_prep(dataframe):
     dataframe = dataframe[dataframe["Price"] > 0]
     return dataframe
 
+
 df = retail_data_prep(df)
+
 
 # ilk olarak, girilen değişkenin %1 çeyrek değerini ve %99 çeyrek değerini hesaplayıp değişkenler de bunu bir tut,
 # sonra diyoruz ki %99 luk çeyrek  değerden 1,5 IQR uzaklıktaki değer benim üst limitimdir,
@@ -50,6 +53,7 @@ def outlier_thresholds(dataframe, variable):
     low_limit = quartile1 - 1.5 * interquantile_range
     return low_limit, up_limit
 
+
 # yukarıda hesaplanan değere göre baskılama yapmak
 # ilgili değişkenden küçük olanlar var mı bak, yukarıda belirlenen low limitten küçük ve ilgili değişkeni, low limite tekrar ata yani baskıla
 # aynı şeyi up limit için de yap
@@ -57,6 +61,7 @@ def replace_with_thresholds(dataframe, variable):
     low_limit, up_limit = outlier_thresholds(dataframe, variable)
     dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
     dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
 
 # yeniden çağırıp yukarıdaki fonksiyonları da ekliyoruz bu sefer
 def retail_data_prep(dataframe):
@@ -68,9 +73,55 @@ def retail_data_prep(dataframe):
     replace_with_thresholds(dataframe, "Price")
     return dataframe
 
+
 df = retail_data_prep(df)
 df.isnull().sum()
 df.describe().T
 
+#########################################################
+# 2. ARL Veri Yapısını Hazırlama (Invoice-Product Matrix)
+#########################################################
+# Genel Amaç= Product Matrix, her bir faturaya ait ürünler(description), sütunlarda yazacak ve o fatura da o ürün varsa 1 yoksa 0
+df.head()
+
+df_fr = df[df["Country"] == "France"]
+df_fr.head()
+
+df.groupby(["Invoice", "Description"]).agg({"Quantity": "sum"}).head(20)
+
+# Description'ları sütuna geçirmek için
+# unstack() diyerek pivot yapıyoruz, sonra da satır ve sütunlardan 5'er tane getir
+df.groupby(["Invoice", "Description"]).agg({"Quantity": "sum"}).unstack().iloc[0:5, 0:5]
+
+# unstack den sonra NAN olan yerleri 0 ile doldurma
+df.groupby(["Invoice", "Description"]).agg({"Quantity": "sum"}).unstack().fillna(0).iloc[0:5, 0:5]
+
+# applaymap ile olan ürünlere 1 olmayanlara 0 yazdırma
+df.groupby(["Invoice", "StockCode"]). \
+    agg({"Quantity": "sum"}). \
+    unstack(). \
+    fillna(0). \
+    applymap(lambda x: 1 if x > 0 else 0).iloc[0:5, 0:5]
 
 
+# id = True olursa yani girilirse yukarıda yapılan işi StockCode'a göre yap
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(["Invoice", "StockCode"])["Quantity"].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(["Invoice", "Description"])["Quantity"].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+fr_inv_pro_df = create_invoice_product_df(df_fr)
+
+fr_inv_pro_df = create_invoice_product_df(df_fr, id=True)
+
+
+# verilen id'ye göre ürün bulma
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
+
+check_id(df_fr, 10002)
+check_id(df_fr, 10120)
