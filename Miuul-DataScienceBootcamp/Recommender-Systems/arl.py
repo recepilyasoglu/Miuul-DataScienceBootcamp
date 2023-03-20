8d772487038a86d2299a43ee2fb8e3baa6977c25
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-20 08:39:56 +0300

    Association Rules Analysis

--- Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
+++ Miuul-DataScienceBootcamp/Recommender-Systems/arl.py
@@ -113,6 +113,7 @@ def create_invoice_product_df(dataframe, id=False):
         return dataframe.groupby(["Invoice", "Description"])["Quantity"].sum().unstack().fillna(0). \
             applymap(lambda x: 1 if x > 0 else 0)
 
+
 fr_inv_pro_df = create_invoice_product_df(df_fr)
 
 fr_inv_pro_df = create_invoice_product_df(df_fr, id=True)
@@ -123,5 +124,81 @@ def check_id(dataframe, stock_code):
     product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
     print(product_name)
 
+
 check_id(df_fr, 10002)
 check_id(df_fr, 10120)
+
+######################################################################
+# 3. Birliktelik Kurallarının Çıkarılması (Association Rules Analysis)
+######################################################################
+
+frequent_itemsets = apriori(fr_inv_pro_df,
+                            min_support=0.01,
+                            use_colnames=True)
+
+# olası ürün çiftleri ve bunların suport değerlerri
+frequent_itemsets.sort_values("support", ascending=False)
+
+rules = association_rules(frequent_itemsets,
+                          metric="support",
+                          min_threshold=0.01)
+
+# belirli şartlara göre indirgedik
+# confidence = bir ürün alındığında diğerinin alınma olasılığı
+rules[(rules["support"] > 0.05) & (rules["confidence"] > 0.1) & (rules["lift"] > 5)]. \
+    sort_values("confidence", ascending=False)
+
+check_id(df_fr, 21080)
+check_id(df_fr, 21086)
+
+
+####################################
+# 4. Çalışmanın Scriptini Hazırlama
+####################################
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(["Invoice", "StockCode"])["Quantity"].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(["Invoice", "Description"])["Quantity"].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
+
+def create_rules(dataframe, id=True, country="France"):
+    dataframe = dataframe[dataframe["Country"] == country]
+    dataframe = create_invoice_product_df(dataframe, id)
+    frequent_itemsets = apriori(dataframe, min_support=0.01, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
+    return rules
+
+df = df_.copy()
+df = retail_data_prep(df)
+rules = create_rules(df)
+
+rules[(rules["support"] > 0.05) & (rules["confidence"] > 0.1) & (rules["lift"] > 5)]. \
+    sort_values("confidence", ascending=False)
