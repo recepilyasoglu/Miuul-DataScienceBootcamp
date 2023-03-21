88fc497d25faa591e8fdcf8ca0410e06bf4a914a
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-21 15:42:11 +0300

    Other Users Watching the Same Movies

--- Miuul-DataScienceBootcamp/Recommender-Systems/ubcf.py
+++ Miuul-DataScienceBootcamp/Recommender-Systems/ubcf.py
@@ -15,7 +15,7 @@
 #############################################
 import pandas as pd
 import numpy as np
-pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 500)
 pd.set_option('display.expand_frame_repr', False)
@@ -33,7 +33,69 @@ def create_user_movie_df():
 
 user_movie_df = create_user_movie_df()
 
+# veri setinden rastgele user seçmek
 random_user = int(pd.Series(user_movie_df.index).sample(1, random_state=45).values)
+
+
+#################################################################################
+# Adım 2: İzlenen Filmleri Getirme Uygulaması(Practical to Bring Watched Movies)
+#################################################################################
+random_user
+user_movie_df
+random_user_df = user_movie_df[user_movie_df.index == random_user]
+
+# user'in puan verdiği filmleri alalım, NAN olmayan
+movies_watched = random_user_df.columns[random_user_df.notna().any()].tolist()
+
+# satırlarında (index) user'i bul, sütunda da yazılı filmi bul (sağlama yapmak için)
+user_movie_df.loc[user_movie_df.index == random_user,
+                  user_movie_df.columns == "Schindler's List (1993)"]
+
+user_movie_df.loc[user_movie_df.index == random_user,
+                  user_movie_df.columns == "Silence of the Lambs, The (1991)"]
+
+user_movie_df.loc[user_movie_df.index == random_user,
+                  user_movie_df.columns == "Sabrina (1995)"]
+
+len(movies_watched)  # toplam izlediği film sayısı
+
+
+######################################################################################
+# Adım 3: Aynı Filmi İzleyen Diğer Kullanıcılar(Other Users Watching the Same Movies)
+######################################################################################
+
+# artık sadece izlenen filmleri aldık
+movies_watched_df = user_movie_df[movies_watched]
+
+# her bir kullanıcın kaç tane film izlediği bilgisi
+user_movie_count = movies_watched_df.T.notnull().sum()
+
+user_movie_count = user_movie_count.reset_index()
+
+user_movie_count.columns = ["userId", "movie_count"]
+
+# user'imiz ile en az 20 ortak filmi izleyen kullanıcılar
+user_movie_count[user_movie_count["movie_count"] > 20].sort_values("movie_count", ascending=False)
+
+# hepsini izleyen kullanıcı
+user_movie_count[user_movie_count["movie_count"] == 33].count()
+
+# en az 20 film izleyen kullanıcıların id bilgileri
+user_movie_count[user_movie_count["movie_count"] > 20]["userId"]
+
+# perc = len(movies_watched) * 60 / 100
+# users_same_movies = user_movie_count[user_movie_count["movie_count"] > perc]["userId"]
+
+
+##########################################################################################
+# Adım 4: Öneri Yapılacak Kullanıcı ile En Benzer Davranışlı Kullanıcıların Belirlenmesi
+##########################################################################################
+
+# Bunun için 3 adım gerçekleştireceğiz:
+# 1. User ve diğer kullanıcıların verilerini bir araya getireceğiz.
+# 2. Korelasyon df'ini oluşturacağız.
+# 3. En benzer bullanıcıları (Top Users) bulacağız
+
 
 
 
