8673424c98d9ef79badefd2b2e4365e639877f98
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-30 23:15:26 +0300

    Outliers

--- Miuul-DataScienceBootcamp/Feature-Engineering/feature_engineering.py
+++ Miuul-DataScienceBootcamp/Feature-Engineering/feature_engineering.py
@@ -194,6 +194,116 @@ for col in num_cols:
     print(col, check_outlier(dff, col))
 
 
+###################
+# Aykırı Değerlerin Kendilerine Erişmek
+###################
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+    # 10 dan çok aykırı değer varsa head ile getir, çok gelmesin yani 10 tane gelsin
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:  # değilse hepsini geir azmış zaten
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+grab_outliers(df, "Age")
+
+grab_outliers(df, "Age", True)  #index bilgilerini de aldık
+
+age_index = grab_outliers(df, "Age", True)
+
+# Yapılanlar
+outlier_thresholds(df, "Age")
+check_outlier(df, "Age")
+grab_outliers(df, "Age", True)
+
+
+#############################################
+# Aykırı Değer Problemini Çözme
+#############################################
+
+###################
+# Silme
+###################
+
+low, up = outlier_thresholds(df, "Fare")
+df.shape
+df.describe().T
+# aykırı olmayanları getir
+df[~((df["Fare"] < low) | (df["Fare"] > up))].shape
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+df.shape
+
+for col in num_cols:
+    new_df = remove_outlier(df, col)
+
+df.shape[0] - new_df.shape[0]
+
+
+#####################################################
+# Baskılama Yöntemi (re-assignment with thresholds)
+#####################################################
+
+low, up = outlier_thresholds(df, "Fare")
+
+df[((df["Fare"] < low) | (df["Fare"] > up))]["Fare"]
+
+df.loc[((df["Fare"] < low) | (df["Fare"] > up)), "Fare"]
+
+df.loc[(df["Fare"] > up), "Fare"] = up
+
+df.loc[(df["Fare"] < low), "Fare"] = low
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+df = load()
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+df.shape
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+
+###################
+# Recap
+###################
+
+df = load()
+outlier_thresholds(df, "Age")
+check_outlier(df, "Age")
+grab_outliers(df, "Age", index=True)
+
+remove_outlier(df, "Age").shape
+replace_with_thresholds(df, "Age")
+check_outlier(df, "Age")
+
+
+
+
 
 
 
