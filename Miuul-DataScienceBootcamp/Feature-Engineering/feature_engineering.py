22f3345a5179ca042788a0ea73303c50be6821d6
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-30 22:59:36 +0300

    Outliers

--- Miuul-DataScienceBootcamp/Feature-Engineering/feature_engineering.py
+++ Miuul-DataScienceBootcamp/Feature-Engineering/feature_engineering.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("Qt5Agg")
 # !pip install missingno
 import missingno as msno
 from datetime import date
@@ -27,12 +29,169 @@ df = load_application_train()
 df.head()
 
 def load():
-    data = pd.read_csv("titanic.csv")
+    data = pd.read_csv("Feature-Engineering/titanic.csv")
+    return data
+
+df = load()
+df.head()
 
 
+#############################################
+# 1. Outliers (Aykırı Değerler)
+#############################################
+
+##############################
+# Aykırı Değerleri Yakalama
+##############################
+
+##################################
+# Grafik Teknikle Aykırı Değerler
+##################################
+
+sns.boxplot(x=df["Age"])
+plt.show()
+
+##################################
+# Aykırı Değerler Nasıl Yakalanır?
+##################################
+
+q1 = df["Age"].quantile(0.25)  # %25 lik değerini getir
+q3 = df["Age"].quantile(0.75)
+
+iqr = q3 - q1
+
+up = q3 + 1.5 * iqr
+low = q1 - 1.5 * iqr
+
+df[(df["Age"] < low) | (df["Age"] > up)]
+
+df[(df["Age"] < low) | (df["Age"] > up)].index
+
+#############################
+# Aykırı DeğerVar mı Yok mu?
+#############################
+
+df[(df["Age"] < low) | (df["Age"] > up)].any(axis=None)  # satır ya da sütuna göre değil hepsine bakmak istediğimizden axis=None
+
+df[(df["Age"] < low)].any(axis=None)
+
+# 1. Eşik değer belirledik.
+# 2. Aykırılara eriştik.
+# 3. Hızlıca aykırı değer var mı yok diye sorduk.
 
 
+################################
+# İşlemleri Fonksiyonlaştırmak
+################################
 
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+outlier_thresholds(df, "Age")
+outlier_thresholds(df, "Fare")
+
+low, up = outlier_thresholds(df, "Fare")
+
+df[(df["Age"] < low) | (df["Age"] > up)].head()
+
+df[(df["Fare"] < low) | (df["Fare"] > up)].index
+
+def check_outlier(dataframe, col_name):  # q1 ve q3 'ü de biçimlendirmek istersek check_outlier'a argüman olarak girmemiz gerekir
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False   # var mı yok mu sorusuna bool dönmesi lazım (True veya False)
+
+check_outlier(df, "Age")
+check_outlier(df, "Fare")
+
+
+###################
+# grab_col_names
+###################
+
+dff = load_application_train()
+dff.head()
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+cat_cols, num_cols, cat_but_car = grab_col_names(dff)
+
+num_cols = [col for col in num_cols if col not in "SK_ID_CURR"]
+
+for col in num_cols:
+    print(col, check_outlier(dff, col))
 
 
 
