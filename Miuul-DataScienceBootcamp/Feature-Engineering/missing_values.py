9aecfbf3e000d13d2f85ee18a8f8c708374ce46b
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-31 17:00:01 +0300

    Missing Values

--- Miuul-DataScienceBootcamp/Feature-Engineering/missing_values.py
+++ Miuul-DataScienceBootcamp/Feature-Engineering/missing_values.py
@@ -69,6 +69,66 @@ df.isnull().sum().sort_values(ascending=False)
 # sadece eksik degerlere sahip degiskenler
 na_cols = [col for col in df.columns if df[col].isnull().sum() > 0]
 
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
 
 def missing_values_table(dataframe, na_name=False):
     na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
@@ -105,9 +165,74 @@ df["Age"].fillna(df["Age"].mean()).isnull().sum()
 df["Age"].fillna(df["Age"].median()).isnull().sum()
 df["Age"].fillna(0).isnull().sum()
 
+# df.apply(lambda x: x.fillna(x.mean()), axis=0)  # satırların ortalamasını alacağımız için axis=0
+
+dff = df.apply(lambda x: x.fillna(x.mean())if x.dtype != "O" else x, axis=0)  # object olmayan değerleri doldur
+
+dff.isnull().sum().sort_values(ascending=False)
+
+# kategorik değişkenleri doldurmak için en iyi yöntem modunu almaktır
+df["Embarked"].fillna(df["Embarked"].mode()[0]).isnull().sum()
+
+df["Embarked"].fillna("missing")
+
+# tipi object olan ve eşsiz değer sayısı 10'dan(yoruma açık) küçük olan,
+# bir kategorik değişken ise bunu modunu değişkene ata
+df.apply(lambda x: x.fillna(x.mode()[0]) if (x.dtype == "O" and len(x.unique()) <= 10) else x, axis=0).isnull().sum()
+
+##############################################
+# Kategorik Değişken Kırılımında Değer Atama
+##############################################
+
+df.groupby("Sex")["Age"].mean()
+
+df["Age"].mean()
+
+df["Age"].fillna(df.groupby("Sex")["Age"].transform("mean")).isnull().sum()
+
+df.groupby("Sex")["Age"].mean()["female"]
+
+# kadınlardaki eksiklikleri, kırılımdaki kadınların ortalaması ile doldurduk
+df.loc[(df["Age"].isnull()) & (df["Sex"] == "female"), "Age"] = df.groupby("Sex")["Age"].mean()["female"]
+
+# erkekler için de aynısını yapıyoruz
+df.loc[(df["Age"].isnull()) & (df["Sex"] == "male"), "Age"] = df.groupby("Sex")["Age"].mean()["male"]
+
+df.isnull().sum()
 
 
+#############################################
+# Çözüm 3: Tahmine Dayalı Atama ile Doldurma
+#############################################
 
+df = load()
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+num_cols = [col for col in num_cols if col not in "PassengerId"]
+
+# get_dummies = sadece kategorik değişkenlere dönüşüm uygulamaktadır,
+# onun için cat_cols'u çğırdık
+dff = pd.get_dummies(df[cat_cols + num_cols], drop_first=True)
+
+dff.head()
+
+# değişkenlerin standartlaştırılması
+scaler = MinMaxScaler()
+dff = pd.DataFrame(scaler.fit_transform(dff), columns=dff.columns)
+dff.head()
+
+# knn'in uygulanması.
+from sklearn.impute import KNNImputer
+imputer = KNNImputer(n_neighbors=5)  # en yakın 5 komşu
+dff = pd.DataFrame(imputer.fit_transform(dff), columns=dff.columns)  # en yakın 5 komşunun dolu olan gözlemlerinin ortalamasını boş olan değerlere atar
+dff.head()
+
+dff = pd.DataFrame(scaler.inverse_transform(dff), columns=dff.columns)  # geri alıyoruz, önceki halleriylekıyas yapamıyoruz çünkü
+
+df["age_imputed_knn"] = dff[["Age"]]
+
+df.loc[df["Age"].isnull(), ["Age", "age_imputed_knn"]]  # veri setimzideki eksik olan değerlerin yerine tahmin edilmiş halini atamış olduk
+df.loc[df["Age"].isnull()]
 
 
 
