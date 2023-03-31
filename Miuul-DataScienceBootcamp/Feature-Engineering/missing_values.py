f90092ebd21a66c74631e98a4efa542f197262e4
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-03-31 16:15:21 +0300

    Missing Values

--- Miuul-DataScienceBootcamp/Feature-Engineering/missing_values.py
+++ Miuul-DataScienceBootcamp/Feature-Engineering/missing_values.py
@@ -42,8 +42,71 @@ df.head()
 df = load()
 df.head()
 
-# eksik gozlem var mı yok mu 
+# eksik gozlem var mı yok mu
 df.isnull().values.any()
+
+# degiskenlerdei eksik deger sayisi
+df.isnull().sum()
+
+# degiskenlerdeki tam deger sayisi
+df.notnull().sum()
+
+# veri setindeki toplam eksik deger sayisi
+df.isnull().sum().sum()
+
+# en az bir tane eksik degere sahip olan gözlem birimleri
+df[df.isnull().any(axis=1)]
+
+# tam olan gözlem birimleri
+df[df.notnull().all(axis=1)]
+
+# Azalan şekilde sıralamak
+df.isnull().sum().sort_values(ascending=False)
+
+# yuzdelik degerlerini gorme, yuzde kaci eksik gibi
+(df.isnull().sum() / df.shape[0] * 100).sort_values(ascending=False)
+
+# sadece eksik degerlere sahip degiskenler
+na_cols = [col for col in df.columns if df[col].isnull().sum() > 0]
+
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+
+    if na_name:
+        return na_columns
+
+missing_values_table(df)
+
+missing_values_table(df, True)
+
+
+#############################################
+# Eksik Değer Problemini Çözme
+#############################################
+
+missing_values_table(df)
+
+###################
+# Çözüm 1: Hızlıca silmek
+###################
+df.dropna().shape
+
+###################
+# Çözüm 2: Basit Atama Yöntemleri ile Doldurmak
+###################
+
+df["Age"].fillna(df["Age"].mean()).isnull().sum()
+df["Age"].fillna(df["Age"].median()).isnull().sum()
+df["Age"].fillna(0).isnull().sum()
+
+
+
 
 
 
