5d1734c60d15e17dba76cdf606b758efa661a4a9
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-02 16:40:45 +0300

    One Hot Encoding

--- Miuul-DataScienceBootcamp/Feature-Engineering/label_encoding.py
+++ Miuul-DataScienceBootcamp/Feature-Engineering/label_encoding.py
@@ -88,3 +88,102 @@ df["Embarked"].nunique()
 len(df["Embarked"].unique())
 
 
+#############################################
+# One-Hot Encoding
+#############################################
+
+df = load()
+df.head()
+df["Embarked"].value_counts()
+
+pd.get_dummies(df, columns=["Embarked"]).head()
+
+# dummy tuzağından kurtulmak için, drop_first=True
+pd.get_dummies(df, columns=["Embarked"], drop_first=True).head()
+
+# nan(eksik) değerleri de değişkene dönüştürme
+pd.get_dummies(df, columns=["Embarked"], dummy_na=True).head()
+
+# get_dummies ile hem label encoding hem one hot encoding yapıyoruz
+pd.get_dummies(df, columns=["Sex", "Embarked"], drop_first=True).head()
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = load()
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+# cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+
+one_hot_encoder(df, ohe_cols).head()
+
+
+
+
+
+
+
+
+
