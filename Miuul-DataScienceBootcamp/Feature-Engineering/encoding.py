e94873ab599de6c3559e727afb3243ee157844ef
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-02 18:35:59 +0300

    Encoding Scaling

--- Miuul-DataScienceBootcamp/Feature-Engineering/encoding.py
+++ Miuul-DataScienceBootcamp/Feature-Engineering/encoding.py
@@ -254,4 +254,61 @@ rare_analyser(new_df, "TARGET", cat_cols)
 df["OCCUPATION_TYPE"].value_counts()
 
 
+#############################################
+# Feature Scaling (Özellik Ölçeklendirme)
+#############################################
 
+###################################################################################################
+# StandardScaler: Klasik standartlaştırma. Ortalamayı çıkar, standart sapmaya böl. z = (x - u) / s
+###################################################################################################
+
+df = load()
+ss = StandardScaler()
+df["Age_standard_scaler"] = ss.fit_transform(df[["Age"]])
+df.head()
+
+
+#########################################
+# RobustScaler: Medyanı çıkar iqr'a böl.
+#########################################
+# aykırı değerlerden etkilenmediği için StanardScaler'e göre farklı sonuç verdi
+# daha az etkilenmiş şekilde standartlaştırma işlemini gerçekleştirdi
+rs = RobustScaler()
+df["Age_robuts_scaler"] = rs.fit_transform(df[["Age"]])
+df.describe().T
+
+###################
+# MinMaxScaler: Verilen 2 değer arasında değişken dönüşümü
+###################
+
+# X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+# X_scaled = X_std * (max - min) + min
+
+mms = MinMaxScaler()
+df["Age_min_max_scaler"] = mms.fit_transform(df[["Age"]])
+df.describe().T
+
+df.head()
+
+# bu değişkenleri kıyaslayabilmek adına
+age_cols = [col for col in df.columns if "Age" in col]
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+for col in age_cols:
+    num_summary(df, col, plot=True)
+
+#############################################################################
+# Numeric to Categorical: Sayısal Değişkenleri Kateorik Değişkenlere Çevirme
+# Binning####################################################################
+###################
+
+df["Age_qcut"] = pd.qcut(df['Age'], 5)
