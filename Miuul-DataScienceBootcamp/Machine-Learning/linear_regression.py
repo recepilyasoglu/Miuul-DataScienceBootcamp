74d32f88a1dfccdb44e2bc4f717939c4e33c8632
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-09 16:45:40 +0300

    Sales Prediction with Linear Regression

--- Miuul-DataScienceBootcamp/Machine-Learning/linear_regression.py
+++ Miuul-DataScienceBootcamp/Machine-Learning/linear_regression.py
@@ -6,8 +6,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import matplotlib
+matplotlib.use("Qt5Agg")
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.set_option('display.width', 500)
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -17,3 +19,76 @@ from sklearn.model_selection import train_test_split, cross_val_score
 ######################################################
 # Simple Linear Regression with OLS Using Scikit-Learn
 ######################################################
+df = pd.read_csv("Machine-Learning/Datasets/advertising.csv")
+df.head()
+df.shape
+
+X = df[["TV"]]  # bağımsız değişken
+y = df[["sales"]]  # bağımlı değişken
+
+
+##########
+# Model
+##########
+reg_model = LinearRegression().fit(X, y)
+
+# y_hat = b + w*TV
+
+# sabit (b - bias)
+reg_model.intercept_[0]
+
+# tv'nin katsayısı (w1)
+reg_model.coef_[0][0]
+
+##########################
+# Tahmin
+##########################
+
+# 150 birimlik TV harcaması olsa ne kadar satış olması beklenir?
+reg_model.intercept_[0] + reg_model.coef_[0][0] * 150
+
+# 500 birimlik tv harcaması olsa ne kadar satış olur?
+reg_model.intercept_[0] + reg_model.coef_[0][0] * 500
+
+df.describe().T
+
+# Modelin Görselleştirilmesi
+g = sns.regplot(x=X, y=y, scatter_kws={'color': 'b', 's': 9},
+                ci=False, color="r")
+
+g.set_title(f"Model Denklemi: Sales = {round(reg_model.intercept_[0], 2)} + TV*{round(reg_model.coef_[0][0], 2)}")
+g.set_ylabel("Satış Sayısı")
+g.set_xlabel("TV Harcamaları")
+plt.xlim(-10, 310)
+plt.ylim(bottom=0)
+plt.show()
+
+##########################
+# Tahmin Başarısı
+##########################
+
+# MSE
+y_pred = reg_model.predict(X)
+mean_squared_error(y, y_pred)
+# 10.51
+y.mean()
+y.std()
+
+# RMSE
+np.sqrt(mean_squared_error(y, y_pred))
+# 3.24
+
+# MAE
+mean_absolute_error(y, y_pred)
+# 2.54
+
+# R-KARE
+reg_model.score(X, y)
+
+
+
+
+
+
+
+
