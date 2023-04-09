2bd861db07ee917a5861e4db5c75b6ba94e9de1e
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-09 16:28:43 +0300

    Sales Prediction with Linear Regression

---
+++ Miuul-DataScienceBootcamp/Machine-Learning/linear_regression.py
@@ -0,0 +1,19 @@
+######################################################
+# Sales Prediction with Linear Regression
+######################################################
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split, cross_val_score
+
+
+######################################################
+# Simple Linear Regression with OLS Using Scikit-Learn
+######################################################
