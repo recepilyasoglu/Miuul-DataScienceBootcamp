c1b220c6ced1b6666aff973d984228e4b62c4537
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-16 02:44:02 +0300

    Rnadom Forests

--- Miuul-DataScienceBootcamp/Machine-Learning/advanced_trees.py
+++ Miuul-DataScienceBootcamp/Machine-Learning/advanced_trees.py
@@ -36,10 +36,72 @@ X = df.drop(["Outcome"], axis=1)
 # Random Forests
 ################################################
 
+rf_model = RandomForestClassifier(random_state=17)
 
+rf_model.get_params()
+# n_estimators = fit edilecek bağımsız ağaç sayısını ifade etmektedir.
 
+cv_results = cross_validate(rf_model, X, y, cv=10, scoring=["accuracy", "f1", "roc_auc"])
+cv_results['test_accuracy'].mean()
+cv_results['test_f1'].mean()
+cv_results['test_roc_auc'].mean()
 
+# bir grid olşturuyoruz (GridSearchCV)
+rf_params = {"max_depth": [5, 8, None],
+             "max_features": [3, 5, 7, "auto"],
+             "min_samples_split": [2, 5, 8, 15, 20],
+             "n_estimators": [100, 200, 500]}
 
+rf_best_grid = GridSearchCV(rf_model, rf_params, cv=5, n_jobs=-1, verbose=True).fit(X, y)
+
+rf_best_grid.best_params_
+
+rf_final = rf_model.set_params(**rf_best_grid.best_params_, random_state=17).fit(X, y)
+
+# neredeyse tüm metrikler de hiperparametre optimizasyonunun sonuç verdiğini görüyoruz
+cv_results = cross_validate(rf_final, X, y, cv=10, scoring=["accuracy", "f1", "roc_auc"])
+cv_results['test_accuracy'].mean()
+cv_results['test_f1'].mean()
+cv_results['test_roc_auc'].mean()
+
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                     ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+plot_importance(rf_final, X)
+
+def val_curve_params(model, X, y, param_name, param_range, scoring="roc_auc", cv=10):
+    train_score, test_score = validation_curve(
+        model, X=X, y=y, param_name=param_name, param_range=param_range, scoring=scoring, cv=cv)
+
+    mean_train_score = np.mean(train_score, axis=1)
+    mean_test_score = np.mean(test_score, axis=1)
+
+    plt.plot(param_range, mean_train_score,
+             label="Training Score", color='b')
+
+    plt.plot(param_range, mean_test_score,
+             label="Validation Score", color='g')
+
+    plt.title(f"Validation Curve for {type(model).__name__}")
+    plt.xlabel(f"Number of {param_name}")
+    plt.ylabel(f"{scoring}")
+    plt.tight_layout()
+    plt.legend(loc='best')
+    plt.show(block=True)
+# arka plan da her bir max_depth parametresi için 500 ağaç fit ediliyor.
+
+val_curve_params(rf_final, X, y, "max_depth", range(1, 11), scoring="roc_auc")
+# Sonuç: max_depth arttıkça train_score da artış olmuş
+# ama validation_score artış gözlemenemedi
 
 
 
