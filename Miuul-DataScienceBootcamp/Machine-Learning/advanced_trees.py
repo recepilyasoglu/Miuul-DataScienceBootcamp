a5c9e90fe0282f36c0c65e38124840fde776db58
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-16 18:46:12 +0300

    CatBoost

--- Miuul-DataScienceBootcamp/Machine-Learning/advanced_trees.py
+++ Miuul-DataScienceBootcamp/Machine-Learning/advanced_trees.py
@@ -244,3 +244,41 @@ cv_results['test_f1'].mean()
 cv_results['test_roc_auc'].mean()
 # 0.8227931516422082
 
+
+################################################
+# CatBoost
+################################################
+
+catboost_model = CatBoostClassifier(random_state=17, verbose=False)
+
+cv_results = cross_validate(catboost_model, X, y, cv=5, scoring=["accuracy", "f1", "roc_auc"])
+
+cv_results['test_accuracy'].mean()
+# 0.7735251676428148
+cv_results['test_f1'].mean()
+# 0.6502723851348231
+cv_results['test_roc_auc'].mean()
+# 0.8378923829489867
+
+# şu haliyle LghtGBM'in son halinden iyi gibi gözüküyor
+
+catboost_params = {"iterations": [200, 500],
+                   "learning_rate": [0.01, 0.1],
+                   "depth": [3, 6]}
+
+
+catboost_best_grid = GridSearchCV(catboost_model, catboost_params, cv=5, n_jobs=-1, verbose=True).fit(X, y)
+
+catboost_best_grid.best_params_
+
+catboost_final = catboost_model.set_params(**catboost_best_grid.best_params_, random_state=17).fit(X, y)
+
+cv_results = cross_validate(catboost_final, X, y, cv=5, scoring=["accuracy", "f1", "roc_auc"])
+
+cv_results['test_accuracy'].mean()
+# 0.7721755368814192
+cv_results['test_f1'].mean()
+# 0.6322580676028952
+cv_results['test_roc_auc'].mean()
+# 0.842001397624039  -> daha önce kullanılan yöntemlerden gelen en iyi auc_score
+
