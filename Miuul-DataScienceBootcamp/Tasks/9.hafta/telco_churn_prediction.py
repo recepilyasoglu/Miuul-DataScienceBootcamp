077ed0c78843fe053e1acaf6eb65869acaad201d
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-18 17:06:13 +0300

    Hyperparameter Optimization on Top 4 Model

--- Miuul-DataScienceBootcamp/Tasks/9.hafta/telco_churn_prediction.py
+++ Miuul-DataScienceBootcamp/Tasks/9.hafta/telco_churn_prediction.py
@@ -4,6 +4,25 @@
 
 ############ Telco Churn Prediction ############
 
+
+# İş Problemi
+
+# Her bir eve ait özelliklerin ve ev fiyatlarının bulunduğu veriseti kullanılarak,
+# farklı tipteki evlerin fiyatlarına ilişkin bir makine öğrenmesi projesi
+# gerçekleştirilmek istenmektedir.
+
+
+
+# Veri Seti Hikayesi
+
+# Ames, Lowa’daki konut evlerinden oluşan bu veri seti içerisinde 79 açıklayıcı değişken bulunduruyor. Kaggle üzerinde bir yarışması
+# da bulunan projenin veri seti ve yarışma sayfasına aşağıdaki linkten ulaşabilirsiniz. Veri seti bir kaggle yarışmasına ait
+# olduğundan dolayı train ve test olmak üzere iki farklı csv dosyası vardır. Test veri setinde ev fiyatları boş bırakılmış olup, bu
+# değerleri sizin tahmin etmeniz beklenmektedir.
+# https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques/overview/evaluation
+
+
+# Importing Libraries
 import warnings
 import matplotlib
 
