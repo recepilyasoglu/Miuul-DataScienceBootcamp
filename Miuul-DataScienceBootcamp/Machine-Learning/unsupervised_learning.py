895a4cbfd35cd460b035d27675ab868c1247a11b
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-26 19:01:21 +0300

    Unsupervised Learning -

--- Miuul-DataScienceBootcamp/Machine-Learning/unsupervised_learning.py
+++ Miuul-DataScienceBootcamp/Machine-Learning/unsupervised_learning.py
@@ -308,3 +308,87 @@ rmse
 
 # Kısaca : önce unsupervised bir yöntem kullanırım, burdan çıkaracağım cluster'lara label muamelesi yaparım,
 # daha sonra bunu bir sınıflandırıcıya sokup eni bir gözlem birimi vs. gediğinde bunu sınıflandırabilirim.
+
+
+#############################################################
+# BONUS: PCA ile Çok Boyutlu Veriyi 2 Boyutta Görselleştirme
+#############################################################
+
+#################
+# Breast Cancer
+#################
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+df = pd.read_csv("Machine-Learning/Datasets/breast_cancer.csv")
+df.head()
+df.shape
+
+y = df["diagnosis"]  # bağımlı değişken
+X = df.drop(["diagnosis", "id"], axis=1)  # bağımsız değişkenler
+
+# 2 bileşene/boyuta indirgeme işlemi
+def create_pca_df(X, y):
+    X = StandardScaler().fit_transform(X)
+    pca = PCA(n_components=2)
+    pca_fit = pca.fit_transform(X)
+    pca_df = pd.DataFrame(data=pca_fit, columns=['PC1', 'PC2'])
+    final_df = pd.concat([pca_df, pd.DataFrame(y)], axis=1)
+    return final_df
+
+pca_df = create_pca_df(X, y)
+
+
+# target değişkenin eşsiz sınıflarını bulacak, bunlar liste halinde tutacak,
+# her bir target'a göre seçin işlemini yapıp iki boyuta göre scatter plot oluşturucak
+# ve bağımlı değişkenleri grafiğin üzerine işaretliyor olacak
+def plot_pca(dataframe, target):
+    fig = plt.figure(figsize=(7, 5))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('PC1', fontsize=15)
+    ax.set_ylabel('PC2', fontsize=15)
+    ax.set_title(f'{target.capitalize()} ', fontsize=20)
+
+    targets = list(dataframe[target].unique())
+    colors = random.sample(['r', 'b', "g", "y"], len(targets))
+
+    for t, color in zip(targets, colors):
+        indices = dataframe[target] == t
+        ax.scatter(dataframe.loc[indices, 'PC1'], dataframe.loc[indices, 'PC2'], c=color, s=50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+
+plot_pca(pca_df, "diagnosis")
+
+
+# farklı veri setleri üzerinde uygulamak istersek
+
+################################
+# Iris
+################################
+
+import seaborn as sns
+df = sns.load_dataset("iris")
+
+y = df["species"]
+X = df.drop(["species"], axis=1)
+
+pca_df = create_pca_df(X, y)
+
+plot_pca(pca_df, "species")
+
+################################
+# Diabetes
+################################
+
+df = pd.read_csv("Machine-Learning/Datasets/diabetes.csv")
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)  # bağımsız değişkenler içerisinde kategorik değişkenler olmaması lazım
+
+pca_df = create_pca_df(X, y)
+
+plot_pca(pca_df, "Outcome")
+# sınıfları birbirinden daha zor ayrılıyor, iç içe geçme durumu var
