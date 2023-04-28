17ccfd054b375be370ca700e72124cf4b998579a
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-28 14:25:20 +0300

    End-to-End Diabetes Machine Learning Pipeline I

---
+++ Miuul-DataScienceBootcamp/Machine-Learning/diabetes_research.py
@@ -0,0 +1,38 @@
+##################################################
+# End-to-End Diabetes Machine Learning Pipeline I
+##################################################
+
+# 1. Exploratory Data Analysis
+# 2. Data Preprocessing & Feature Engineering
+# 3. Base Models
+# 4. Automated Hyperparameter Optimization
+# 5. Stacking & Ensemble Learning
+# 6. Prediction for a New Observation
+# 7. Pipeline Main Function
+
+import joblib
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+
+# !pip install catboost
+# !pip install lightgbm
+# !pip install xgboost
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+################################################
+# 1. Exploratory Data Analysis
+################################################
