e554a4bfdfe8a1101d0b6518794ad95f61248aa7
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-04-28 16:32:01 +0300

    End-to-End Diabetes Machine Learning Pipeline I - Data Preprocessing & Feature Engineering

--- Miuul-DataScienceBootcamp/Machine-Learning/diabetes_research.py
+++ Miuul-DataScienceBootcamp/Machine-Learning/diabetes_research.py
@@ -235,10 +235,83 @@ for col in cat_cols:
 # bağımlı değişkeni çıkarıyoruz
 cat_cols = [col for col in cat_cols if "OUTCOME" not in col]
 
+# one hot encode
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+check_df(df)
+
+df.columns = [col.upper() for col in df.columns]
+
+# Son güncel değişken türlerimi tutuyorum.
+cat_cols, num_cols, cat_but_car = grab_col_names(df, cat_th=5, car_th=20)
+cat_cols = [col for col in cat_cols if "OUTCOME" not in col]
+
+# outlier var mı yok mu
+for col in num_cols:
+    print(col, check_outlier(df, col, 0.05, 0.95))
+
+# INSULIN'de çıktı, replace'ledik
+replace_with_thresholds(df, "INSULIN")
 
 
+# Standartlaştırma
+X_scaled = StandardScaler().fit_transform(df[num_cols])
+df[num_cols] = pd.DataFrame(X_scaled, columns=df[num_cols].columns)
 
+y = df["OUTCOME"]  # bağımlı değişkenimiz
+X = df.drop(["OUTCOME"], axis=1)  # bağımsız değişkenler
 
+check_df(X)
+
+# şimsiye kadar ki süreci, fonksiyonlaştırma
+def diabetes_data_prep(dataframe):
+    dataframe.columns = [col.upper() for col in dataframe.columns]
+
+    # Glucose
+    dataframe['NEW_GLUCOSE_CAT'] = pd.cut(x=dataframe['GLUCOSE'], bins=[-1, 139, 200], labels=["normal", "prediabetes"])
+
+    # Age
+    dataframe.loc[(dataframe['AGE'] < 35), "NEW_AGE_CAT"] = 'young'
+    dataframe.loc[(dataframe['AGE'] >= 35) & (dataframe['AGE'] <= 55), "NEW_AGE_CAT"] = 'middleage'
+    dataframe.loc[(dataframe['AGE'] > 55), "NEW_AGE_CAT"] = 'old'
+
+    # BMI
+    dataframe['NEW_BMI_RANGE'] = pd.cut(x=dataframe['BMI'], bins=[-1, 18.5, 24.9, 29.9, 100],
+                                        labels=["underweight", "healty", "overweight", "obese"])
+
+    # BloodPressure
+    dataframe['NEW_BLOODPRESSURE'] = pd.cut(x=dataframe['BLOODPRESSURE'], bins=[-1, 79, 89, 123],
+                                            labels=["normal", "hs1", "hs2"])
+
+    cat_cols, num_cols, cat_but_car = grab_col_names(dataframe, cat_th=5, car_th=20)
+
+    cat_cols = [col for col in cat_cols if "OUTCOME" not in col]
+
+    df = one_hot_encoder(dataframe, cat_cols, drop_first=True)
+
+    df.columns = [col.upper() for col in df.columns]
+
+    cat_cols, num_cols, cat_but_car = grab_col_names(df, cat_th=5, car_th=20)
+
+    cat_cols = [col for col in cat_cols if "OUTCOME" not in col]
+
+    replace_with_thresholds(df, "INSULIN")
+
+    X_scaled = StandardScaler().fit_transform(df[num_cols])
+    df[num_cols] = pd.DataFrame(X_scaled, columns=df[num_cols].columns)
+
+    y = df["OUTCOME"]
+    X = df.drop(["OUTCOME"], axis=1)
+
+    return X, y
+
+df = pd.read_csv("Machine-Learning/Datasets/diabetes.csv")
+
+check_df(df)
+
+X, y = diabetes_data_prep(df)
+
+check_df(X)
 
 
 
