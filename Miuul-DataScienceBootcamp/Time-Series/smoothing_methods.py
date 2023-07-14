27fea72f305b15ac2f0a8ced26333ad9ac4f9347
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-07-14 17:01:18 +0300

    Smothing Methods (Holt-Winters)

--- Miuul-DataScienceBootcamp/Time-Series/smoothing_methods.py
+++ Miuul-DataScienceBootcamp/Time-Series/smoothing_methods.py
@@ -62,3 +62,61 @@ test = y["1998-01-01":]
 len(test)  # 48 ay
 
 
+###################################################################
+# Zaman Serisi Yapısal Analizi ( Time Series-Structural Analysis) #
+###################################################################
+
+def is_stationary(y):
+
+    # "H0: Non-stationary"
+    # "H1: Stationary
+
+    p_value = sm.tsa.stattools.adfuller(y)[1]
+    if p_value < 0.05:
+        print(F"Result: Stationary (H0: non-stationary, p-value: {round(p_value, 3)})")
+    else:
+        print(F"Result: Non-stationary (H0: non-stationary, p-value: {round(p_value, 3)})")
+
+is_stationary(y)  # istatiki olarak da bu serinin durağan olmadığı bilgisine erişimiş olduk
+
+# Zaman Serisi Bileşenleri ve Durağanlık Testi
+def ts_decompose(y, model="additive", stationary=False):
+    result = seasonal_decompose(y, model=model)
+    fig, axes = plt.subplots(4, 1, sharex=True, sharey=False)
+    fig.set_figheight(10)
+    fig.set_figwidth(15)
+
+    axes[0].set_title("Decomposition for " + model + " model")
+    axes[0].plot(y, 'k', label='Original ' + model)
+    axes[0].legend(loc='upper left')
+
+    axes[1].plot(result.trend, label='Trend')
+    axes[1].legend(loc='upper left')
+
+    axes[2].plot(result.seasonal, 'g', label='Seasonality & Mean: ' + str(round(result.seasonal.mean(), 4)))
+    axes[2].legend(loc='upper left')
+
+    axes[3].plot(result.resid, 'r', label='Residuals & Mean: ' + str(round(result.resid.mean(), 4)))
+    axes[3].legend(loc='upper left')
+    plt.show(block=True)
+
+    if stationary:
+        is_stationary(y)
+
+ts_decompose(y, stationary=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
