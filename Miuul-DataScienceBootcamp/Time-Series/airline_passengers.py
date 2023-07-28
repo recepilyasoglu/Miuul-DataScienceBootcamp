332af877ac856d90a2db50e818c9163a441199f4
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-07-28 10:04:55 +0300

    Airline Passenger Forecasting

---
+++ Miuul-DataScienceBootcamp/Time-Series/airline_passengers.py
@@ -0,0 +1,59 @@
+#################################
+# Airline Passenger Forecasting
+#################################
+
+import itertools
+import warnings
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import statsmodels.api as sm
+import statsmodels.tsa.api as smt
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.metrics import mean_absolute_error
+warnings.filterwarnings('ignore')
+
+
+#################################
+# Verinin Görselleştirilmesi
+#################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
