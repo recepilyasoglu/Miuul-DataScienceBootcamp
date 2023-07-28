d411d768b0bc2f5a6b0f6db15332e8e68077be5e
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-07-28 10:38:46 +0300

    Final Model

--- Miuul-DataScienceBootcamp/Time-Series/airline_passengers.py
+++ Miuul-DataScienceBootcamp/Time-Series/airline_passengers.py
@@ -217,14 +217,55 @@ mean_absolute_error(test, y_pred)
 plot_prediction(pd.Series(y_pred, index=test.index), "SARIMA")
 
 
+# MAE
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+def sarima_optimizer_mae(train, pdq, seasonal_pdq):
+    best_mae, best_order, best_seasonal_order = float("inf"), float("inf"), None
+
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                model = SARIMAX(train, order=param, seasonal_order=param_seasonal)
+                sarima_model = model.fit(disp=0)
+                y_pred_test = sarima_model.get_forecast(steps=24)
+                y_pred = y_pred_test.predicted_mean
+                mae = mean_absolute_error(test, y_pred)
+
+                # mae = fit_model_sarima(train, val, param, param_seasonal)
+
+                if mae < best_mae:
+                    best_mae, best_order, best_seasonal_order = mae, param, param_seasonal
+                print('SARIMA{}x{}12 - MAE:{}'.format(param, param_seasonal, mae))
+            except:
+                continue
+    print('SARIMA{}x{}12 - MAE:{}'.format(best_order, best_seasonal_order, best_mae))
+    return best_order, best_seasonal_order
+
+best_order, best_seasonal_order = sarima_optimizer_mae(train, pdq, seasonal_pdq)
+
+model = SARIMAX(train, order=best_order, seasonal_order=best_seasonal_order)
+sarima_final_model = model.fit(disp=0)
+
+y_pred_test = sarima_final_model.get_forecast(steps=24)
+y_pred = y_pred_test.predicted_mean
+
+mean_absolute_error(test, y_pred)
+# 30.623362595882828
+
+plot_prediction(pd.Series(y_pred, index=test.index), "SARIMA")
+
+# şuana kadar ki modeller arasında en iyisi tes modeli, mae: 11
 
 
+# Final Model (bütün veriyle model kurma)
 
+tes_model_final = ExponentialSmoothing(df, trend="add", seasonal="add", seasonal_periods=12).\
+            fit(smoothing_level=best_alpha, smoothing_slope=best_beta, smoothing_seasonal=best_gamma)
 
-
-
-
-
-
-
+# henüz gerçekleşmemiş 6 adım sonrasına tahmin çalışması
+tes_model_final.forecast(6)
 
