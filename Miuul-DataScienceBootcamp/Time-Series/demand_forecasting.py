d4cf19b2af9079c03e34e5bb73fe4006b161cd0c
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-07-29 10:19:51 +0300

    Demand Forecasting

---
+++ Miuul-DataScienceBootcamp/Time-Series/demand_forecasting.py
@@ -0,0 +1,51 @@
+#####################################################
+# Demand Forecasting
+#####################################################
+
+# Store Item Demand Forecasting Challenge
+# https://www.kaggle.com/c/demand-forecasting-kernels-only
+
+# Amaç mağaza ürün kırılımında 3 ay sonrasını tahmin etmek
+
+import matplotlib
+matplotlib.use("Qt5Agg")
+import time
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import lightgbm as lgb
+import warnings
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+warnings.filterwarnings('ignore')
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
